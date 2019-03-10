44ab691bdcaa0a2431a09ce6771f8cea0a27061e
bertw2002 <awan00@stuy.edu>
2019-03-10 18:00:19 -0400

    finished code, haven't tested

--- partition.java
+++ partition.java
@@ -1,9 +1,30 @@
 public class partition{
+  public static void switchPlace(int[] data, int first, int second){
+    int temp = first;
+    data[first] = data[second];
+    data[second] = data[temp];
+  }
   public static int partition (int [] data, int start, int end){
-    
-
-
-}
-
-
+    int randnum = (int)(Math.random() * (end - start) + start);
+    int partit = data[randnum];
+    boolean startTrue = false;
+    boolean endTrue = false;
+    while (start < end){
+      if (data[start] < partit){
+        start++;
+      }else{
+        startTrue = true;
+      }
+      if (data[end] > partit){
+        end--;
+      }else{
+        endTrue = true;
+      }
+      if ((endTrue && startTrue) && start < end){
+        switchPlace(data, start, end);
+      }
+    }
+    //return last position of partition.
+    return end;
+  }
 }
